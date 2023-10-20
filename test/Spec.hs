501802019b719de4482ec1b3ddc968c3c0100532
mmzk1526 <yc4120@ic.ac.uk>
2023-10-20 12:28:48 +0100

    [mmzk] (test) Check block header hash

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,20 @@
 import           Control.Monad
+import           Class.BCHashable
 import           Model.BCEnv
 import           Model.Block
 import           Model.BlockHeader
 import           Model.Transaction
 import           Test.Hspec
 import           Workflow.LoadEnv
+import Class.BCShow
 
 main :: IO ()
 main = hspec do
   env <- runIO getBCEnv
+  runIO $ putStrLn $ bcShow (last env.blockchains).header
+  describe "Block Hash Tests" do
+    forM_ env.blockchains \b -> it ("Block " ++ show b.header.height) do
+      bcHash b.header `shouldBe` b.header.hash
   describe "Merkle Tests" do
     forM_ env.blockchains \b -> it ("Block " ++ show b.header.height) do
       mkMerkle b.transactions `shouldBe` b.header.transactions_merkle_root
